1b946e5a81282a0893122ec838ebe95d039a9edf
dan525555 <77828669+dan525555@users.noreply.github.com>
2023-11-28 18:55:20 +0100

    Change search

--- src/search.py
+++ src/search.py
@@ -1,17 +1,24 @@
 from app import app, items_db
-from flask import request, jsonify, send_file
+from flask import request, jsonify
 from bson.objectid import ObjectId
-from flask_jwt_extended import jwt_required, current_user
+from flask_jwt_extended import jwt_required
 from utils import serialize_object_ids
 
-
-@app.route("/find/<text>", methods=["PUT"])
+@app.route("/find", methods=["PUT"])
 @jwt_required()  # Requires a valid JWT token
-def find(text):
-    items = items_db.find({"name": "/" + text + "/i" })
-    if items:
-        return jsonify(
-            {"items": serialize_object_ids(items), "message": "Success", "code": 200}
-        )
-    else:
-    	return jsonify({"message": "No items found", "code": 404})
+def find():
+    try:
+        # Pobierz parametr "text" z ciała zapytania PUT
+        text = request.get_json().get("text", "")
+        
+        # Użyj regexa do wyszukiwania na podstawie przekazanego tekstu
+        items = items_db.find({"name": {"$regex": f".*{text}.*", "$options": "i"}})
+        
+        if items:
+            return jsonify(
+                {"items": serialize_object_ids(items), "message": "Success", "code": 200}
+            )
+        else:
+            return jsonify({"message": "No items found", "code": 404})
+    except Exception as e:
+        return jsonify({"message": str(e), "code": 500})
