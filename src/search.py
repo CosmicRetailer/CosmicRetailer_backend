288efc45de6c322ae9147af0ff2dbf6afd68dbcf
dan525555 <77828669+dan525555@users.noreply.github.com>
2023-11-28 21:57:18 +0100

    test v4

--- src/search.py
+++ src/search.py
@@ -6,6 +6,11 @@ from utils import serialize_object_ids
 
 @app.route("/find/<text>", methods=["GET"])
 @jwt_required()  # Requires a valid JWT token
-def search(text):
-  
-        return jsonify({"message": f"No items found for text: {text}", "code": 404})
+def find(text):
+    items = items_db.find({"name": {"$regex": f".*{text}.*", "$options": "i"}})
+    if items:
+        return jsonify(
+            {"items":  items, "message": "Success", "code": 200}
+        )
+    else:
+        return jsonify({"message": "No items found", "code": 404})
