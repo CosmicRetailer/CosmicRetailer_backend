661538faeed4997fa93db9235786147bf3b0c6a4
dan525555 <77828669+dan525555@users.noreply.github.com>
2023-11-28 19:02:19 +0100

    Change to get

--- src/search.py
+++ src/search.py
@@ -4,21 +4,13 @@ from bson.objectid import ObjectId
 from flask_jwt_extended import jwt_required
 from utils import serialize_object_ids
 
-@app.route("/find", methods=["PUT"])
+@app.route("/find/<text>", methods=["GET"])
 @jwt_required()  # Requires a valid JWT token
-def find():
-    try:
-        # Pobierz parametr "text" z ciała zapytania PUT
-        text = request.get_json().get("text", "")
-        
-        # Użyj regexa do wyszukiwania na podstawie przekazanego tekstu
-        items = items_db.find({"name": {"$regex": f".*{text}.*", "$options": "i"}})
-        
-        if items:
-            return jsonify(
-                {"items": serialize_object_ids(items), "message": "Success", "code": 200}
-            )
-        else:
-            return jsonify({"message": "No items found", "code": 404})
-    except Exception as e:
-        return jsonify({"message": str(e), "code": 500})
+def find(text):
+    items = items_db.find({"name": {"$regex": f".*{text}.*", "$options": "i"}})
+    if items:
+        return jsonify(
+            {"items": serialize_object_ids(items), "message": "Success", "code": 200}
+        )
+    else:
+        return jsonify({"message": "No items found", "code": 404})
