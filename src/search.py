c438577e34f8e0b56eed0c22065addf2d904c46f
dan525555 <77828669+dan525555@users.noreply.github.com>
2023-11-28 21:35:49 +0100

    test v2

--- src/search.py
+++ src/search.py
@@ -4,8 +4,8 @@ from flask import request, jsonify
 from flask_jwt_extended import jwt_required
 from utils import serialize_object_ids
 
-@app.route("/find/<text>", methods=["GET"])
+@app.route("/find", methods=["GET"])
 @jwt_required()  # Requires a valid JWT token
-def search(text):
+def search():
     
     return jsonify({"message": "No items found", "code": 404})
