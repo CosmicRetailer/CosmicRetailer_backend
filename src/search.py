a9964071fdad0387ae499c90635d4257c6fc25f8
dan525555 <77828669+dan525555@users.noreply.github.com>
2023-11-28 21:13:27 +0100

    Change search

--- src/search.py
+++ src/search.py
@@ -1,16 +1,18 @@
+import json
 from app import app, items_db
 from flask import request, jsonify
-from bson.objectid import ObjectId
+from bson import ObjectId
 from flask_jwt_extended import jwt_required
-from utils import serialize_object_ids
+from utils import serialize_object_ids, convert_to_json_serializable
 
 @app.route("/find/<text>", methods=["GET"])
 @jwt_required()  # Requires a valid JWT token
-def find(text):
+def search(text):
     items = items_db.find({"name": {"$regex": f".*{text}.*", "$options": "i"}})
     if items:
+        serialized_items = serialize_object_ids(items)
         return jsonify(
-            {"items": serialize_object_ids(items), "message": "Success", "code": 200}
+            {"items": json.loads(json.dumps(serialized_items, default=convert_to_json_serializable)), "message": "Success", "code": 200}
         )
     else:
         return jsonify({"message": "No items found", "code": 404})
