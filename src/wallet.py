0ca2998ec89bbb0095d2ea879ebfe80172986353
cpprian <cpprian456@gmail.com>
2023-11-30 22:42:09 +0100

    update /buy_item

--- src/wallet.py
+++ src/wallet.py
@@ -19,6 +19,10 @@ def getBalance():
 @app.route('/buy_item/<item_id>', methods=['POST'])
 @jwt_required()
 def buyItem(item_id):
+    pvk = request.json.get("privateKey")
+    if pvk is None:
+        return jsonify({'message': 'can\'t buy an item', "code": 400})
+
     item = items_db.find_one({'_id': ObjectId(item_id)})
     if item is None:
         return jsonify({'message': 'item not found', "code": 404})
@@ -62,7 +66,7 @@ def buyItem(item_id):
     }
     gas = web3.eth.estimate_gas(tx)
     tx['gas'] = gas
-    signed_tx = web3.eth.account.sign_transaction(tx, app.config["INFURA_PRIVATE_KEY"])
+    signed_tx = web3.eth.account.sign_transaction(tx, pvk)
     tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
     print("Transaction hash: " + str(web3.to_hex(tx_hash)))
