c374ace97a35d9fa4017ccd784bba45e5c163c7b
LogicalInsanity <idkwtw223@gmail.com>
2023-12-04 12:57:35 +0100

    search extended

--- src/search.py
+++ src/search.py
@@ -17,3 +17,17 @@ def find(text):
         )
 
     return jsonify({"message": "Items not found", "code": 404})
+
+
+@app.route("/find_tag/<text>", methods=["GET"])
+@jwt_required()  # Requires a valid JWT token
+def find_tag(text):
+    items = items_db.find({"category": {"$regex": f".*{text}.*", "$options": "i"}})
+    items_serializable = json.loads(json.dumps(list(items), default=convert_to_json_serializable))
+
+    if items:
+        return jsonify(
+            {"items": items_serializable, "message": "Success", "code": 200}
+        )
+
+    return jsonify({"message": "Items not found", "code": 404})
